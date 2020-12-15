7955a56c1c24a166478b2fd9deda768b471ffb4e
felipebit <felipebit>
2020-12-14 21:08:53 -0300

    baseURL changed

--- src/components/scratch-buddy/api.js
+++ src/components/scratch-buddy/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://18.188.92.227:3000/' // 'http://localhost:3333'
+    baseURL: 'http://18.188.92.227:3000/api/' // 'http://localhost:3333'
 });
 
 export default api;
